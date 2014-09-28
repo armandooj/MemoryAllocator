06032a5578a86ca044d302ea040d10ec7c658393
Armando Ochoa <armando.o.j@gmail.com>
2014-09-28 13:22:45 +0200

    Make memory_alloc allocate one single block, still inefficient

--- mem_alloc.c
+++ mem_alloc.c
@@ -32,42 +32,60 @@ free_block_t first_free;
 #define max(x,y) (x>y?x:y)
 
 
-/*INITIALIZE THE LIST OF FREEBLOCK WHIT A single free block
-corresponding to the full array
+/*
+Initialize the list of free blocks with a single free block corresponding to the full array.
 */
 void memory_init(void) {
-	first_free=(free_block_s*)memory;
-	first_free->size=MEMORY_SIZE-sizeof(free_block_s);
-	first_free->next=NULL;
+	first_free = (void *)memory;
+  printf("memory: %d\n", (int)first_free);
+	first_free->size = MEMORY_SIZE - sizeof(free_block_s);
+	first_free->next = NULL;
 }
 
+/*
+This method allocates a block of size size. It returns a pointer to the allocated memory or NULL if the allocation failed.
+*/
 char *memory_alloc(int size) {
-	int real_size=size+sizeof(free_block_s);
-	free_block_t current;
-	/*LOOP IN THE LIST TO FIND A BLOCK OF RIGHT SIZE*/
-    for(current = first_free; current != NULL; current = current->next){
-		if(current->size > real_size){
-			/*FOUNDED --- >ALLOCATION*/
-			if(current==first_free){
-				first_free->next=first_free->next + real_size; //We moove the pointer!
-				first_free->size=first_free->size - real_size;
-				return (char*)current->next;
-			}
-			else { 
-				//print_alloc_info(addr, actual_size); 
-				return (char*)32;
-			}
+  // the size needed for both memory and header
+	int real_size = size + sizeof(busy_block_s);  
+	free_block_t current, previous;  
+
+  // printf("busy_block size: %lu, real_size: %d\n", sizeof(busy_block_s), real_size);
+
+  for (current = first_free; current != NULL; current = current->next) {
+    // We found an empty block with enough space
+    // First fit criteria
+		if (current->size > real_size) {                    
+        // It was the first free block
+        if (current == first_free) {
+          first_free = first_free + real_size; // We moove the pointer!
+          first_free->size = first_free->size - real_size;  
+        }
+        // Somewhere else, link the previous to the next one 
+        else {          
+          previous->next = current->next; 
+        }
+
+        // Create a busy block
+        busy_block_t new_busy_block = (busy_block_t)current;
+        new_busy_block->size = (current->size + sizeof(free_block_s)) - sizeof(busy_block_s);
+			             
+       printf("current: %d\n", (int)current); // NOTE this looks weird, why? (maybe virtual address)
+      return (char *)((int)current + sizeof(busy_block_s));	    
 		}
-		else {
-						printf("NO");
-		return NULL;
-		}
+    previous = current;
 	}
-return NULL;
+
+  return NULL;
 }
 
-void memory_free(char *p){
-  print_free_info(p); 
+/*
+This method frees the zone adressed by zone. whose address is given receives an address to an occupied block. 
+It updates the list of the free blocks and merge contiguous blocks.
+*/
+void memory_free(char *p) {
+  // print_free_info(p); 
+
 
   /* ... */
 }
@@ -100,7 +118,7 @@ void print_free_blocks(void) {
   free_block_t current; 
   fprintf(stderr, "Begin of free block list :\n"); 
   for(current = first_free; current != NULL; current = current->next)
-    fprintf(stderr, "Free block at address %lu, size %u\n", ULONG((char*)current - memory), current->size);
+    fprintf(stderr, "Free block at address %lu, size %u\n", ULONG((char*)current-memory ), current->size);
 }
 
 char *heap_base(void) {
@@ -145,16 +163,17 @@ int main(int argc, char **argv){
 
   /* The main can be changed, it is *not* involved in tests */
   memory_init();
-  print_info(); 
-  print_free_blocks();
-  memory_alloc(111);
-  /* int i ; 
-  for( i = 0; i < 10; i++){
-    char *b = memory_alloc(rand()%8);
+  // printf("malloc: %d\n", (int)memory_alloc(1));
+
+  int i ; 
+  for( i = 0; i < 1; i++){
+    char *b = memory_alloc(16);//memory_alloc(rand()%8);
+    printf("b: %d\n", (int)b);
     memory_free(b); 
     print_free_blocks();
   }
 
+  /*
   char * a = memory_alloc(15);
   a=realloc(a, 20); 
   memory_free(a);
